082dd9cd4d9f41ac238e41deeb0e043728f2c672
vladislav <vladislavbailovic@gmail.com>
2016-05-25 19:46:35 +0200

    Fix the example script.

--- snitch/index.js
+++ snitch/index.js
@@ -1,34 +1,37 @@
-const electron = require('electron');
+var electron = require('electron');
+
 // Module to control application life.
-const {app} = electron;
-// Module to create native browser window.
-const {BrowserWindow} = electron;
-const {Tray} = electron;
+var app = electron.app,
+	// Module to create native browser window.
+	BrowserWindow = electron.BrowserWindow,
+ 	Tray = electron.Tray
+;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
-let appTray;
+var win,
+	appTray
+;
 
 function createWindow() {
 	// Create the browser window.
 	win = new BrowserWindow({width: 800, height: 600});
 
 	// and load the index.html of the app.
-	win.loadURL(`file://${__dirname}/html/index.html`);
+	win.loadURL('file://' + __dirname + '/html/index.html');
 
 	// Open the DevTools.
-	win.webContents.openDevTools();
+	//win.webContents.openDevTools();
 
 	// Emitted when the window is closed.
-	win.on('closed', () => {
+	win.on('closed', function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
 		win = null;
 	});
 
-	appTray = new Tray(`${__dirname}/img/g2.png`);
+	appTray = new Tray(__dirname + '/img/g2.png');
 	appTray.setToolTip("test test");
 }
 
@@ -38,21 +41,12 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+app.on('window-all-closed', function () {
+	app.quit();
 });
 
-app.on('activate', () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
+app.on('activate', function () {
 	if (win === null) {
 		createWindow();
 	}
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.

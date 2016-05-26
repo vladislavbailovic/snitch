1858b41f8d513e25b0fa4af3cc37d2a582c0c992
vladislav <vladislavbailovic@gmail.com>
2016-05-26 10:47:07 +0200

    Re-creating tray minimized window.

--- snitch/index.js
+++ snitch/index.js
@@ -17,29 +17,32 @@ var win,
 ;
 
 function createWindow() {
-	// Create the browser window.
-	win = new BrowserWindow({
-		icon: __dirname + '/img/g2.png'
-	});
-	win.maximize();
+	var _create = function () {
+		// Create the browser window.
+		win = new BrowserWindow({
+			icon: __dirname + '/img/g2.png'
+		});
+		win.maximize();
 
-	// and load the index.html of the app.
-	win.loadURL('file://' + __dirname + '/html/index.html');
+		// and load the index.html of the app.
+		win.loadURL('file://' + __dirname + '/html/index.html');
 
-	// Open the DevTools.
-	//win.webContents.openDevTools();
+		// Open the DevTools.
+		//win.webContents.openDevTools();
 
-	// Emitted when the window is closed.
-	win.on('closed', function () {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		win = null;
-	});
+		// Emitted when the window is closed.
+		win.on('closed', function () {
+			// Dereference the window object, usually you would store windows
+			// in an array if your app supports multi windows, this is the time
+			// when you should delete the corresponding element.
+			//win = null;
+		});
 
-	win.on('focus', function () {
-		appTray.setImage(__dirname + '/img/g2.png');
-	});
+		win.on('focus', function () {
+			appTray.setImage(__dirname + '/img/g2.png');
+		});
+	}
+	_create();
 
 	ctxMenu = Menu.buildFromTemplate([
 		{label: 'Exit', type: 'radio', click: function () {
@@ -50,6 +53,10 @@ function createWindow() {
 	appTray = new Tray(__dirname + '/img/g2.png');
 	appTray.setToolTip("test test");
 	appTray.setContextMenu(ctxMenu);
+
+	appTray.on("click", function () {
+		_create();
+	});
 
 	Ipc.on('new-line', function (e, txt) {
 		appTray.setToolTip(txt);
@@ -68,7 +75,7 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-	app.quit();
+	//app.quit();
 });
 
 app.on('activate', function () {

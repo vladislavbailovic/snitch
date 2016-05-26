1244b48985469c18ad1e8c07af6ff7d8ac60e73d
vladislav <vladislavbailovic@gmail.com>
2016-05-26 21:12:27 +0200

    Add more logs items markup.

--- snitch/js/template.js
+++ snitch/js/template.js
@@ -8,10 +8,32 @@
 				'<li data-id="<%= index %>">',
 					'<%= data.name %>',
 					'<div class="meta">',
-						'<div><a href="#clear">Clear</a></div>',
-						'<div><a href="#path">Path</a></div>',
-						'<div><a href="#show-only">Show only</a></div>',
-						'<div><a href="#show-except">Show except</a></div>',
+
+						'<div class="clear meta-item">',
+							'<a href="#clear">Clear</a>',
+						'</div>',
+
+						'<div class="path meta-item">',
+							'<a href="#path">Path</a>',
+							'<div class="meta-content">',
+								'<input type="text" />',
+							'</div>',
+						'</div>',
+
+						'<div class="only meta-item">',
+							'<a href="#show-only">Show only</a>',
+							'<div class="meta-content">',
+								'<textarea></textarea>',
+							'</div>',
+						'</div>',
+
+						'<div class="except meta-item">',
+							'<a href="#show-except">Show except</a>',
+							'<div class="meta-content">',
+								'<textarea></textarea>',
+							'</div>',
+						'</div>',
+
 					'</div>',
 				'</li>'
 			].join('')),

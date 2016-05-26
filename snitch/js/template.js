d4fb1dd17b6f829dc7ae049cd16c3b7c40834035
vladislav <vladislavbailovic@gmail.com>
2016-05-26 21:38:25 +0200

    Add some basic styles.

--- snitch/js/template.js
+++ snitch/js/template.js
@@ -6,31 +6,35 @@
 		Logs: {
 			Item: template([
 				'<li data-id="<%= index %>">',
-					'<%= data.name %>',
+					'<h4><%= data.name %></h4>',
 					'<div class="meta">',
 
-						'<div class="clear meta-item">',
+						'<div class="meta-actions">',
 							'<a href="#clear">Clear</a>',
+							' ',
+							'<a href="#clear">Pause</a>',
+							' ',
+							'<a href="#kill">Kill</a>',
 						'</div>',
 
 						'<div class="path meta-item">',
 							'<a href="#path">Path</a>',
 							'<div class="meta-content">',
-								'<input type="text" />',
+								'<input type="text" value="<%= data.file %>" />',
 							'</div>',
 						'</div>',
 
 						'<div class="only meta-item">',
 							'<a href="#show-only">Show only</a>',
 							'<div class="meta-content">',
-								'<textarea></textarea>',
+								'<textarea><%= data.only_condition %></textarea>',
 							'</div>',
 						'</div>',
 
 						'<div class="except meta-item">',
 							'<a href="#show-except">Show except</a>',
 							'<div class="meta-content">',
-								'<textarea></textarea>',
+								'<textarea><%= data.except_condition %></textarea>',
 							'</div>',
 						'</div>',
 

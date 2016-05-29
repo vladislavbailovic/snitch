5ceffa599a904bc92765807184ef2956cac2cef9
vladislav <vladislavbailovic@gmail.com>
2016-05-29 08:30:54 +0200

    Add some new templates.

--- snitch/js/template.js
+++ snitch/js/template.js
@@ -2,45 +2,67 @@
 
 	var template = require('lodash.template'); // https://www.npmjs.com/package/lodash.template
 
-	module.exports = {
-		Logs: {
-			Item: template([
-				'<li data-id="<%= index %>">',
-					'<h4><%= data.name %></h4>',
-					'<div class="meta">',
+	function get_item_meta () {
+		return [
+			'<div class="name meta-item">',
+				'<a href="#path">Name</a>',
+				'<div class="meta-content">',
+					'<input name="name" type="text" value="<%= data.name %>" />',
+				'</div>',
+			'</div>',
 
-						'<div class="meta-actions">',
-							'<a href="#clear"><span>Clear</span></a>',
-							' ',
-							'<a href="#pause"><span>Pause</span></a>',
-							' ',
-							'<a href="#kill"><span>Kill</span></a>',
-						'</div>',
+			'<div class="path meta-item">',
+				'<a href="#path">Path</a>',
+				'<div class="meta-content">',
+					'<input name="file" type="text" value="<%= data.file %>" />',
+				'</div>',
+			'</div>',
 
-						'<div class="path meta-item">',
-							'<a href="#path">Path</a>',
-							'<div class="meta-content">',
-								'<input name="path" type="text" value="<%= data.file %>" />',
-							'</div>',
-						'</div>',
+			'<div class="only meta-item">',
+				'<a href="#show-only">Show only</a>',
+				'<div class="meta-content">',
+					'<textarea name="only_condition"><%= data.only_condition %></textarea>',
+				'</div>',
+			'</div>',
 
-						'<div class="only meta-item">',
-							'<a href="#show-only">Show only</a>',
-							'<div class="meta-content">',
-								'<textarea name="only"><%= data.only_condition %></textarea>',
-							'</div>',
-						'</div>',
+			'<div class="except meta-item">',
+				'<a href="#show-except">Show except</a>',
+				'<div class="meta-content">',
+					'<textarea name="except_condition"><%= data.except_condition %></textarea>',
+				'</div>',
+			'</div>',
+		].join('');
+	}
 
-						'<div class="except meta-item">',
-							'<a href="#show-except">Show except</a>',
-							'<div class="meta-content">',
-								'<textarea name="except"><%= data.except_condition %></textarea>',
-							'</div>',
-						'</div>',
+	function get_item () {
+		return [
+			'<li data-id="<%= index %>">',
+				'<h4><%= data.name %></h4>',
+				'<div class="meta">',
 
+					'<div class="meta-actions">',
+						'<a href="#clear"><span>Clear</span></a>',
+						' ',
+						'<a href="#pause"><span>Pause</span></a>',
+						' ',
+						'<a href="#kill"><span>Kill</span></a>',
 					'</div>',
-				'</li>'
-			].join('')),
+
+					get_item_meta(),
+
+				'</div>',
+			'</li>'
+		].join('');
+	}
+
+	var Tpl = {
+		Logs: {
+			Item: template(get_item()),
+			AddNew: template([
+				get_item_meta(),
+				'<div class="save"><button><span>Save</span></button></div>',
+				'<div class="cancel"><button><span>Cancel</span></button></div>',
+			].join(''))
 		},
 		Out: {
 			Item: template([
@@ -51,4 +73,7 @@
 			].join(''))
 		}
 	};
+
+	module.exports = Tpl;
+
 })();

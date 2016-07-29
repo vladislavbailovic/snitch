39a98c8f12a16e7894824f9b4c594b1841bd7860
vladislav <vladislavbailovic@gmail.com>
2016-07-29 08:16:46 +0200

    Attempt parsing raw log line into a message object.

--- snitch/js/postprocess.js
+++ snitch/js/postprocess.js
@@ -1,18 +1,50 @@
 ;(function (undefined) {
 	"use strict";
-	
+
 	function format_structure (data) {
 		return JSON.stringify(data, null, "\t");
 	}
 
+	/**
+	 * Attempts parsing raw log line into message object
+	 *
+	 * A message object contains separate headers and message properties
+	 *
+	 * @param {String} txt Raw log message
+	 *
+	 * @return {Object} Log message object
+	 */
+	function parse_log_line (txt) {
+		var res = [],
+			msg = {
+				header: '',
+				message: ''
+			}
+		;
+
+		res = txt.match(/^\[([^\]]+)\]\s*(.*?)$/); // Attempt to match raw log headers
+		if (res && 3 === res.length) {
+			msg.header = res[1];
+			msg.message = res[2];
+		}
+
+		return msg;
+	}
+
 	function json_decode (txt) {
-		var data = false;
+		var data = false,
+			msg = parse_log_line(txt)
+		;
+
+		if (!msg.message) return txt;
 
 		try {
-			data = JSON.parse(txt);
+			data = JSON.parse(msg.message);
 		} catch (e) {}
 
-		return data ? format_structure(data) : txt;
+		if (!data) return txt;
+
+		return '[' + msg.header + ']' + format_structure(data);
 	}
 
 	module.exports = {
